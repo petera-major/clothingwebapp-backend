a0fd8862b74c410f8323dacc21338ede3464e007
tera <peteramajor@MacBookPro.lan>
2025-06-18 18:30:42 -0400

    Allow Netlify frontend domain in CORS

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://envisionyurtaste.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
